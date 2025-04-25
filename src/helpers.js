c0bcb278471d1c84520cac02427fe6a89679972d
samliburd <liburdsam@gmail.com>
2025-04-25 21:40:43 +0100

    added PATH logic

--- src/helpers.js
+++ src/helpers.js
@@ -1,21 +1,66 @@
 const { core, console, http, utils, exec } = iina;
 
-const FFMPEG_URL =
-  "https://github.com/eugeneware/ffmpeg-static/releases/download/b6.0/ffmpeg-darwin-arm64";
+const FFMPEG_URL = "https://evermeet.cx/ffmpeg/get/ffmpeg/zip";
+const FFMPEG_ZIP_PATH = utils.resolvePath("@tmp/ffmpeg.zip");
+const BINARY_PATH = utils.resolvePath("@data/bin/");
+
+function logger(msg) {
+  console.log(msg);
+  core.osd(msg);
+}
 
 export async function downloadFFMPEG() {
   try {
-    await http.download(FFMPEG_URL, "@data/ffmpeg");
+    await http.download(FFMPEG_URL, FFMPEG_ZIP_PATH);
+    console.log(`ffmpeg downloaded to ${FFMPEG_ZIP_PATH}`);
     return null;
   } catch (e) {
+    console.log(e);
     return e.toString();
   }
 }
 
-export async function makeExecutable() {
-  const resolvedPath = utils.resolvePath("@data/ffmpeg");
-  return await utils.exec("/bin/chmod", ["+x", resolvedPath]);
+export async function getFfmpegPath() {
+  try {
+    const { status, stdout, stderr } = await utils.exec("/bin/bash", [
+      "-c",
+      "export PATH=$PATH:/opt/homebrew/bin:/usr/local/bin:/opt/local/bin && command -v ffmpeg",
+      // "echo $PATH",
+    ]);
+    if (status === 0) {
+      logger(stdout);
+    } else {
+      logger(`Output: ${stdout} ${stderr}`);
+    }
+  } catch (error) {
+    logger(`Could not find ffmpeg`);
+  }
 }
+
+export async function unzip() {
+  try {
+    // Execute the unzip command
+    const { status, stdout, stderr } = await utils.exec("/usr/bin/unzip", [
+      "-o",
+      `${FFMPEG_ZIP_PATH}`,
+      "-d",
+      `${BINARY_PATH}`,
+    ]);
+
+    // Check the exit status of the command
+    if (status === 0) {
+      // If the command was successful, display success message
+      logger(`ffmpeg extracted to ${BINARY_PATH}/ffmpeg`);
+    } else {
+      // If there's a non-zero status, consider it an error and display stderr
+      logger(`Error extracting ffmpeg: ${stderr || stdout}`);
+    }
+  } catch (error) {
+    // If there's an exception during the command execution, display the error
+    logger(`Unzip failed: ${error.message}`);
+  }
+}
+
 // export function chmod() {}
 export function findBinary() {
   let path = "ffmpeg";
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { standaloneWindow, overlay, console, menu, core, input, utils, file } =
   iina;
-import { downloadFFMPEG, findBinary, makeExecutable } from "./helpers";
+import * as helpers from "./helpers";
 // Initialize overlay
 overlay.loadFile("dist/ui/overlay/index.html");
 overlay.setClickable(true);
@@ -217,49 +217,49 @@ subTimeMenu.addSubMenuItem(
     { keyBinding: "Meta+u" },
   ), // Meta (Command) + u
 );
-// subTimeMenu.addSubMenuItem(
+menu.addItem(subTimeMenu);
+const subDownloadMenu = menu.item("Download");
+// subDownloadMenu.addSubMenuItem(
 //   menu.item("Log time", () => {
 //     // file.write("@data/mynewfile.txt", "hello");
 //     downloadFFMPEG();
 //     findBinary();
 //   }), // Meta (Command) + u
 // );
-subTimeMenu.addSubMenuItem(
+
+subDownloadMenu.addSubMenuItem(
   menu.item("Download", () => {
     // file.write("@data/mynewfile.txt", "hello");
-    downloadFFMPEG();
+    helpers.downloadFFMPEG();
   }), // Meta (Command) + u
 );
-subTimeMenu.addSubMenuItem(
-  menu.item("Resolve", () => {
-    const resolvedPath = utils.resolvePath("@data/ffmpeg");
+subDownloadMenu.addSubMenuItem(
+  menu.item("Unzip", () => {
+    const ffmpegZip = utils.resolvePath("@data/ffmpeg.7z");
+    const binDir = utils.resolvePath("@data/bin");
     // (async () => {
-    //   const { status, stdout, stderr } = await utils.exec("/bin/chmod", [
-    //     "+x",
-    //     resolvedPath,
-    //   ]);
+    //   const { status, stdout, stderr } = await utils.exec(
+    //     "/opt/homebrew/bin/7z",
+    //     ["e", `${ffmpegZip}`, `-o${binDir}/ffmpeg`],
+    //   );
+    //   core.osd(stderr);
+    //   console.log(stderr);
     // })();
-    makeExecutable()
-      .then(({ status, stdout, stderr }) => {
-        if (status === 0) {
-          core.osd(`chmod succeeded: ${stdout}`);
-        } else {
-          core.osd(`chmod failed: ${stderr}`);
-        }
-      })
-      .catch((err) => {
-        core.osd(`Error running chmod: ${err}`);
-      });
-    core.osd(resolvedPath);
+    helpers.unzip();
   }), // Meta (Command) + u
 );
-subTimeMenu.addSubMenuItem(
+subDownloadMenu.addSubMenuItem(
+  menu.item("Resolve", () => {
+    helpers.getPath();
+  }), // Meta (Command) + u
+);
+subDownloadMenu.addSubMenuItem(
   menu.item("Find binary", () => {
     // file.write("@data/mynewfile.txt", "hello");
-    findBinary();
+    helpers.findBinary();
   }), // Meta (Command) + u
 );
-menu.addItem(subTimeMenu);
+menu.addItem(subDownloadMenu);
 menu.addItem(
   menu.item(
     "Toggle crop",
